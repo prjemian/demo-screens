f72725aaca3c66deeeadf3f94386d451d933f070
Pete Jemian <prjemian@gmail.com>
2020-03-20 09:36:49 -0500

    TST fixes #1

--- caQtDM-screen-demo.sh
+++ caQtDM-screen-demo.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-export CAQTDM_DISPLAY_PATH=.:/tmp:${HOME}/Documents/projects/demo-screens/caqtdm
+# export CAQTDM_DISPLAY_PATH=.:/tmp:${HOME}/Documents/projects/demo-screens/caqtdm
+export CAQTDM_DISPLAY_PATH=.:/tmp:${HOME}/Documents/projects/demo-screens/medm
 export CAQTDM_OPTIMIZE_EPICS3CONNECTIONS=true
 export QT_PLUGIN_PATH=/usr/local/epics/extensions/lib/linux-x86_64
-caQtDM -style plastique -noMsg screen1.ui
+# caQtDM -style plastique -noMsg screen1.ui
+caQtDM -style plastique -noMsg screen1.adl
